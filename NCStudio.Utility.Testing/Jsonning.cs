13c8be35ba8c2c5b33cd09b494bc507ed574fcce
NickChan9394 <nickchan9394@live.cn>
2017-10-30 16:33:52 +0800

    add Jsonning

---
+++ NCStudio.Utility.Testing/Jsonning.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NCStudio.Utility.Testing
+{
+    public static class Jsonning
+    {
+        public static string SerializeJsonObjectWithCamelCasePropertyNames(object target)
+        {
+            return JsonConvert.SerializeObject(target, Formatting.None, new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                DateTimeZoneHandling = DateTimeZoneHandling.Local
+            });
+        }
+
+        public static JObject ConvertToCamelCasePropertyJObject(object target)
+        {
+            return JObject.Parse(JsonConvert.SerializeObject(target, Formatting.None, new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                DateTimeZoneHandling = DateTimeZoneHandling.Local
+            }));
+        }
+
+        public static JObject ConvertToJObject(object target)
+        {
+            return JObject.Parse(JsonConvert.SerializeObject(target, Formatting.None, new JsonSerializerSettings()
+            {
+                DateTimeZoneHandling = DateTimeZoneHandling.Local
+            }));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,10 @@ This project contains common utilities for coding and testing.
 
 # Utilites:
 
-+ Mocking.GetMockDbSet\<T\> : Get a DbSet mocking object for testing.
+### Mocking
++ `DbSet<T> GetMockDbSet<T>(List<T> sourceList)` : Get a DbSet mocking object for testing.
+
+### Jsonning
++ `string SerializeJsonObjectWithCamelCasePropertyNames(object target)`: Serial an object to json string with camel case properties.
++ `JObject ConvertToCamelCasePropertyJObject(object target)`:Convert an object to JObject with camel case properties.
++ `JObject ConvertToJObject(object target)`:Convert an object to JObject
