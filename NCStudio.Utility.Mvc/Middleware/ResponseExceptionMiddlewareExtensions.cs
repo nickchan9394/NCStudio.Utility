f3d253fb0551055d55dbccbc316c9ea988dd9bcc
NickChan9394 <nickchan9394@live.cn>
2017-10-31 21:10:18 +0800

    add ResponseExceptionMiddleware.cs and related files

---
+++ NCStudio.Utility.Mvc/Middleware/ResponseExceptionMiddlewareExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NCStudio.Utility.Mvc.Middleware
+{
+    public static class ResponseExceptionMiddlewareExtensions
+    {
+        /// <summary>
+        /// Exception string and 500 status Code will be returned if an exception throws inside the server
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseResponseException(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ResponseExceptionMiddleware>();
+        }
+    }
+}
