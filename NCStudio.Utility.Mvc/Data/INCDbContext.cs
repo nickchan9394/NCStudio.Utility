e9a2362745a5e2ee97e2a3c2b136fa3b8a6d33c5
NickChan9394 <nickchan9394@live.cn>
2017-11-06 11:22:18 +0800

    add soem DbContext interfaces to INCDbContext

--- NCStudio.Utility.Mvc/Data/INCDbContext.cs
+++ NCStudio.Utility.Mvc/Data/INCDbContext.cs
@@ -4,6 +4,9 @@ namespace NCStudio.Utility.Mvc.Data
 {
     public interface INCDbContext
     {
+        EntityEntry Add(object entity);
+        int SaveChanges();
+
         void LoadNavigationProperty(EntityEntry entry, params string[] props);
         void MarkAsDeleted<T>(T entity) where T : class;
     }
