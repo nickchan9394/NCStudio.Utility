d81f55170d40da858c12b9ea62cf48d6a8bfbdf7
NickChan9394 <nickchan9394@live.cn>
2017-11-16 15:48:42 +0800

    add IUnitOfWork to NCDbContext

--- NCStudio.Utility.Mvc/Data/NCDbContext.cs
+++ NCStudio.Utility.Mvc/Data/NCDbContext.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using NCStudio.Core;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NCStudio.Utility.Mvc.Data
 {
-    public class NCDbContext : DbContext, INCDbContext
+    public class NCDbContext : DbContext, INCDbContext,IUnitOfWork
     {
         public NCDbContext() { }
         public NCDbContext(DbContextOptions options) : base(options) { }
@@ -22,6 +24,32 @@ namespace NCStudio.Utility.Mvc.Data
         public void MarkAsDeleted<T>(T entity) where T : class
         {
             this.Entry<T>(entity).State = EntityState.Deleted;
+        }
+
+        public async Task RunTransactionAsync(Func<Task> transactionFunc)
+        {
+            using (var transactionBeginTask = Database.BeginTransactionAsync())
+            using (var transaction = await transactionBeginTask)
+            {
+                await transactionFunc();
+                transaction.Commit();
+            }
+        }
+
+        public async Task<TResult> RunTransactionAsync<TResult>(Func<Task<TResult>> transactionFunc)
+        {
+            using (var transactionBeginTask = Database.BeginTransactionAsync())
+            using (var transaction = await transactionBeginTask)
+            {
+                var result = await transactionFunc();
+                transaction.Commit();
+                return result;
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            await SaveChangesAsync();
         }
     }
 }
