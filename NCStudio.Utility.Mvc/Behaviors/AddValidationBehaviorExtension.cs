4741e34ab06fdfd049708def5ced221aa9b1f8dc
NickChan9394 <nickchan9394@live.cn>
2017-11-16 11:26:21 +0800

    add add-behavior-extensions

---
+++ NCStudio.Utility.Mvc/Behaviors/AddValidationBehaviorExtension.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NCStudio.Utility.Mvc.Behaviors
+{
+    public static class AddValidationBehaviorExtension
+    {
+        public static void AddNCValidationBehavior(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        }
+    }
+}
