08791fef69b26a9ccdac8efb092e389a512b5393
NickChan9394 <nickchan9394@live.cn>
2017-11-16 11:18:26 +0800

    add behaviors

---
+++ NCStudio.Utility.Mvc/Behaviors/ExceptionBehavior.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NCStudio.Utility.Mvc.Behaviors
+{
+    public class ExceptionBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+        where TResponse :class
+    {
+        public ExceptionBehavior()
+        {
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (ValidationException ve)
+            {
+                return new BadRequestObjectResult(ve.ToString()) as TResponse;
+            }
+        }
+    }
+}
---
+++ NCStudio.Utility.Mvc/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NCStudio.Utility.Mvc.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private IValidator<TRequest>[] _validators;
+
+        public ValidationBehavior(IValidator<TRequest>[] validators)
+        {
+            _validators = validators??throw new ArgumentNullException(nameof(IValidator<TRequest>));
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next)
+        {
+            var failures = _validators
+                .Select(v => v.Validate(request))
+                .SelectMany(result => result.Errors)
+                .Where(error => error != null)
+                .ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException($"Command Validation Errors for type { typeof(TRequest).Name} : {String.Join("\n",failures.Select(f=>f.ErrorMessage))}"
+                    , failures);
+            }
+
+            return next();
+        }
+    }
+}
