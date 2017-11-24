0bd86da7b3e73dccd6971dcd85c7f24cb1a6e4a4
NickChan9394 <nickchan9394@live.cn>
2017-11-24 10:20:45 +0800

    add security functions and extensions

---
+++ NCStudio.Utility.Security/JsonWebToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NCStudio.Utility.Security
+{
+    public class JsonWebToken
+    {
+        public string AccessToken { get; set; }
+        public int ExpiresIn { get; set; }
+    }
+}
---
+++ NCStudio.Utility.Security/SignMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NCStudio.Utility.Security
+{
+    public enum SignMethods
+    {
+        MD5,
+        SHA1
+    }
+}
