a70df9b7076584cf99dae9a0987b84d23804ac46
NickChan9394 <nickchan9394@live.cn>
2017-11-24 10:59:56 +0800

    add jwt cookie to http request

--- NCStudio.Utility.Testing/HttpTesting.cs
+++ NCStudio.Utility.Testing/HttpTesting.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+using NCStudio.Utility.Security;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
@@ -8,10 +11,33 @@ namespace NCStudio.Utility.Testing
 {
     public static class HttpTesting
     {
+
         public async static Task<ResponseResult> GetAsync(string uri)
         {
             using (var httpClient = new HttpClient())
             {
+                var response = await httpClient.GetAsync(uri);
+
+                var result = await response.Content.ReadAsStringAsync();
+                return new ResponseResult
+                {
+                    Content = result,
+                    StatusCode = response.StatusCode
+                };
+            }
+        }
+
+        public async static Task<ResponseResult> GetAsync(string uri,
+            string username,string[] roles,
+            string cookieDomain = "",
+            KeyValuePair<string,string>[] clamins=null,
+            string secretKey = "1234567890123456",
+            string audience="LegoUser",string issuer="Lego")
+        {
+            using (var httpClient = new HttpClient())
+            {
+                addTokenInCookie(httpClient, username, roles, cookieDomain, clamins, secretKey, audience, issuer);
+
                 var response = await httpClient.GetAsync(uri);
 
                 var result = await response.Content.ReadAsStringAsync();
@@ -39,10 +65,64 @@ namespace NCStudio.Utility.Testing
             }
         }
 
+        public async static Task<ResponseResult> PostAsync(
+            string uri, 
+            string content, 
+            string username,
+            string[] roles,
+            string cookieDomain = "",
+            KeyValuePair<string, string>[] clamins = null,
+            Encoding encoding = null, 
+            string mediaType = "application/json",
+            string secretKey = "1234567890123456",
+            string audience = "LegoUser", string issuer = "Lego")
+        {
+            using (var httpClient = new HttpClient())
+            {
+                addTokenInCookie(httpClient, username, roles, cookieDomain, clamins, secretKey, audience, issuer);
+
+                var response = await httpClient
+                    .PostAsync(uri, new StringContent(content, encoding ?? Encoding.UTF8, mediaType));
+
+                var result = await response.Content.ReadAsStringAsync();
+                return new ResponseResult
+                {
+                    Content = result,
+                    StatusCode = response.StatusCode
+                };
+            }
+        }
+
         public async static Task<ResponseResult> PutAsync(string uri, string content, Encoding encoding = null, string mediaType = "application/json")
         {
             using (var httpClient = new HttpClient())
             {
+                var response = await httpClient
+                    .PutAsync(uri, new StringContent(content, encoding ?? Encoding.UTF8, mediaType));
+
+                var result = await response.Content.ReadAsStringAsync();
+                return new ResponseResult
+                {
+                    Content = result,
+                    StatusCode = response.StatusCode
+                };
+            }
+        }
+
+        public async static Task<ResponseResult> PutAsync(string uri, string content,
+            string username,
+            string[] roles,
+            string cookieDomain = "",
+            KeyValuePair<string, string>[] clamins = null,
+            Encoding encoding = null,
+            string mediaType = "application/json",
+            string secretKey = "1234567890123456",
+            string audience = "LegoUser", string issuer = "Lego")
+        {
+            using (var httpClient = new HttpClient())
+            {
+                addTokenInCookie(httpClient, username, roles, cookieDomain, clamins, secretKey, audience, issuer);
+
                 var response = await httpClient
                     .PutAsync(uri, new StringContent(content, encoding ?? Encoding.UTF8, mediaType));
 
@@ -68,6 +148,69 @@ namespace NCStudio.Utility.Testing
                     StatusCode = response.StatusCode
                 };
             }
+        }
+
+        public async static Task<ResponseResult> DeleteAsync(string uri,
+            string username, string[] roles,
+            string cookieDomain = "",
+            KeyValuePair<string, string>[] clamins = null,
+            string secretKey = "1234567890123456",
+            string audience = "LegoUser", string issuer = "Lego")
+        {
+            using (var httpClient = new HttpClient())
+            {
+                addTokenInCookie(httpClient, username, roles, cookieDomain, clamins, secretKey, audience, issuer);
+
+                var response = await httpClient.DeleteAsync(uri);
+
+                var result = await response.Content.ReadAsStringAsync();
+                return new ResponseResult
+                {
+                    Content = result,
+                    StatusCode = response.StatusCode
+                };
+            }
+        }
+
+        private static void addTokenInCookie(HttpClient httpClient, string username, string[] roles,
+                string cookieDomain,
+                KeyValuePair<string, string>[] clamins,
+                string secretKey,
+                string audience, string issuer)
+        {
+            httpClient.DefaultRequestHeaders.Remove("Cookie");
+            var signingKey = SigningKey.GetSigningKey(secretKey);
+            var options = new TokenProviderOptions
+            {
+                Audience = audience,
+                Issuer = issuer,
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256),
+            };
+            var jwtService = new JwtService();
+
+            JsonWebToken token;
+            if (clamins != null)
+            {
+                token = jwtService.GenerateJsonWebToken(username, roles, options, clamins);
+            }
+            else
+            {
+                token = jwtService.GenerateJsonWebToken(username, roles, options);
+            }
+
+
+            var cookieOptions = new CookieOptions
+            {
+                Domain = cookieDomain,
+                Expires = DateTimeOffset.UtcNow.AddHours(8).AddDays(1).AddMinutes(-5)
+            };
+
+            var cookies = new List<string> {
+                $"access_token={token.AccessToken}",
+                $"username={username}",
+                $"expires_in={token.ExpiresIn}"
+            };
+            httpClient.DefaultRequestHeaders.Add("Cookie", string.Join(";", cookies));
         }
     }
 }
